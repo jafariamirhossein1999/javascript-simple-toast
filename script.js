2c398e313538bc0fc05fe8f0bb20366b8d2c50e8
Amirhossein Jafari <68869122+jafariamirhossein1999@users.noreply.github.com>
2022-10-21 02:40:24 +0330

    first-official-initialize

--- script.js
+++ script.js
@@ -2,26 +2,71 @@
 let _config = {
    duration: 200
 }
-
 class toast {
-   show(message, myduration = 300) {
-      let MyToast = document.querySelector('.toast-message');
-      MyToast.innerText = message;
-      MyToast.classList.add('toast--visible')
-      setTimeout(() => {
-         MyToast.style.paddingLeft = "100px";
-         MyToast.style.paddingRight = "100px";
-         MyToast.style.borderBottom = "solid 10px red";
-      }, 100)
-   this._destroy(MyToast, myduration);
+constructor(message,duration,type){
+this._message = message;
+this._duration = duration;
+this._type = type;
    }
-   _destroy(MyToast, myduration) {
+
+   show() {
+      let MyToast = document.querySelector('.toast-message');
+  switch(this._type){
+case "information":
+
+   MyToast.innerText = this._message;
+   MyToast.classList.add('toast--visible')
+   setTimeout(() => {
+      MyToast.style.paddingLeft = "100px";
+      MyToast.style.paddingRight = "100px";
+  
+   }, 100)
+this._destroy(MyToast, this._duration);
+break;
+case "success":
+   MyToast.innerText = this._message;
+   MyToast.classList.add('toast--visible')
+   MyToast.style.backgroundColor = "#4CAF50"
+   setTimeout(() => {
+      MyToast.style.paddingLeft = "100px";
+      MyToast.style.paddingRight = "100px";
+  
+   }, 100)
+this._destroy(MyToast, this._duration);
+break;
+case "warning":
+   MyToast.innerText = this._message;
+   MyToast.classList.add('toast--visible')
+   MyToast.style.backgroundColor = "#ffeb3b"
+   setTimeout(() => {
+      MyToast.style.paddingLeft = "100px";
+      MyToast.style.paddingRight = "100px";
+  
+   }, 100)
+this._destroy(MyToast, this._duration);
+break;
+case "danger":
+   MyToast.innerText = this._message;
+   MyToast.classList.add('toast--visible')
+   MyToast.style.backgroundColor = "#ff5722"
+   setTimeout(() => {
+      MyToast.style.paddingLeft = "100px";
+      MyToast.style.paddingRight = "100px";
+  
+   }, 100)
+this._destroy(MyToast, this._duration);
+break;
+default:
+   alert('hello world');
+  }
+   }
+   _destroy(MyToast) {
 
       setTimeout(() => {
          MyToast.classList.remove('toast--visible')
          MyToast.style.paddingLeft = "16px";
          MyToast.style.paddingRight = "16px";
-         MyToast.style.borderBottom = "solid 2px red";
-      }, myduration)
+   
+      }, this._duration)
    }
 }
